784d1f478e3761f7620a7cd8b1dc61882bbd9cb4
yangyang-bit <2451624503@qq.com>
2020-11-14 22:57:31 +0800

    ggg

--- js/index.js
+++ js/index.js
@@ -68,6 +68,41 @@ window.addEventListener("scroll", function () {
   }
 });
 
+
+//1.创建一个scrollReveal对象
+window.page1 = ScrollReveal();
+//2.调用scrollReveal里的reveal方法将scrollReveal内部的动画和css中的标签关联起来
+sr.reveal('.',config);
+//参数一：跟html标签关联的类名
+//参数二：动画配置
+var config = {
+  reset: true,   // 滚动鼠标时，动画开关(默认是false没有打开鼠标滚动的动画开关)
+  origin: 'right', // 动画开始的方向
+  duration: 500,   // 动画持续时间
+  delay: 0, // 延迟
+  rotate: {x:0, y:0, z:0},  // 过度到0的初始角度
+  opacity: 0.2, // 初始透明度  (0.2到1的效果)
+  scale:2,       //缩放
+  easing: 'ease-in-out', //动画效果// 缓动'ease', 'ease-in-out'，'linear'...
+  // 回调函数
+//   //当动画开始之前会被触发
+//   beforeReveal: function(domEl){
+//       console.log('动画执行了');
+//   },
+//   //鼠标滚轮滚动之前会被触发
+//   beforeReset: function(domEl){
+//       console.log('滚轮开始---');
+//   },
+//   //动画开始之后会被触发
+//   afterReveal: function(domEl){
+//       console.log('动画结束了');
+//   },
+//   //滚轮滚动之后会被触发
+//   afterReset: function(domEl){
+//       console.log('滚轮结束了');
+//   }
+// };
+
 //page2.js
 
 //page3.js
