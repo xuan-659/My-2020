086346dd162f93ce15a561e2a8fcde71ab35917f
YANCHN <838666054@qq.com>
2020-11-15 15:45:41 +0800

    fff

--- js/index.js
+++ js/index.js
@@ -31,9 +31,9 @@ function getElementTop(element) {
     scrolly=window.scrollY-sy;
     
          if(scrolly>=0)
-         {angle+=scrolly*0.08;}
+         {angle+=scrolly*0.09;}
          if(scrolly<0)
-         {angle+=scrolly*0.08;}
+         {angle+=scrolly*0.09;}
     
   if(angle > 180){
     // 这里的gap就是每次累加的值
@@ -182,6 +182,8 @@ window.addEventListener("scroll", function () {
 // page4.js
 window.onload = function () {
   var z = 0;
+  var hengx=0;
+  var lastx=0;
   function mouseup() {
     alert("qq");
   }
@@ -189,40 +191,36 @@ window.onload = function () {
   var isdown = 1;
   var p2 = document.getElementsByClassName("phto1")[0];
   app.onmousedown = function (e) {
-    var x = e.pageX * 0.05;
-    app.style.cursor = "grabbing";
-    console.log(x);
-    if (x >= 25) {
-      z = z + x * 0.5;
+    hengx=e.pageX;
+    app.style.cursor="grabbing";
+    isdown=1;
+    window.onmousemove=function(a){
+        if(isdown){
+            // p2.style.transition=1+"s";
+            var left =a.pageX;
+            var px=(left-hengx)*0.1;
+           p2.style.marginLeft= lastx+px+"vw";
+            if((lastx+px)<=-520){
+                p2.style.transition=0+"s";
+                p2.style.marginLeft= 0+"vw";
+                lastx=0;
+            }
+            else{
+                p2.style.transition=1+"s";
+            }
+            if((lastx+px)>=100){
+                lastx=0;
+            }
+            console.log(lastx+px);
+       window.onmouseup=function(a){
+           isdown=0;
+           app.style.cursor="grab";
+           lastx+=px;
+           console.log("1")             
+       }
     }
-    if (x < 25) {
-      z = z - x * 2;
-    }
-    isdown = 1;
-    app.onmousemove = function (a) {
-      if (isdown) {
-        // p2.style.transition=1+"s";
-        var left = a.pageX;
-        var px = (x - left) * 0.001;
-        p2.style.marginLeft = -z + "vw";
-        console.log(z);
-        if (z >= 520) {
-          p2.style.transition = 0 + "s";
-          p2.style.marginLeft = 0 + "vw";
-          z = 0;
-        } else {
-          p2.style.transition = 1 + "s";
-        }
-        if (z <= -50) {
-          p2.style.marginLeft = 0 + "vw";
-        }
-        // console.log(p2.style.marginRight);
-      }
-      window.onmouseup = function (a) {
-        isdown = 0;
-        app.style.cursor = "grab";
-      };
-    };
+}
+   
   };
 };
 
